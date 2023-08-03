657e6d1b13bcf5f60a5c5491c946fa14853f773f
lmuhamadaslan <muhamadaslan22@gmail.com>
2023-08-03 15:15:00 +0800

    initial commit

---
+++ app/Http/Controllers/api/ObatController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\ObatModel;
+use App\Models\GudangbarangModel;
+use Illuminate\Http\Request;
+
+class ObatController extends Controller
+{
+    public function index()
+    {
+        $data = ObatModel::join('jenis', 'databarang.kdjns', '=', 'jenis.kdjns')
+            ->join('industrifarmasi', 'industrifarmasi.kode_industri', '=', 'databarang.kode_industri')
+            ->join('gudangbarang', 'databarang.kode_brng', '=', 'gudangbarang.kode_brng')
+            ->join('resep_dokter', 'resep_dokter.kode_brng', '=', 'databarang.kode_brng')
+            ->select('databarang.kode_brng', 'databarang.nama_brng', 'jenis.nama', 'databarang.kode_sat', 'databarang.h_beli', 'databarang.letak_barang', 'industrifarmasi.nama_industri', 'databarang.h_beli', 'gudangbarang.stok', 'resep_dokter.jml', 'resep_dokter.aturan_pakai')
+            ->get();
+        return response()->json([
+            'success' => true,
+            'message' => 'Daftar data obat',
+            'data' => $data
+        ], 200);
+    }
+}
